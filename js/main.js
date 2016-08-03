498edc057a1b31841564b2a93e3d8a3c1a868277
salus-sage <salus.sage@gmail.com>
2016-08-04 02:15:34 +0530

    Refactor: #1 add more comments

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 //Main entry to app -> like app init???? not just world init,
-//which shud include business logic
+//which shud include business logic main.js and hotspot.js can be changes as required
 //TODO: create context for this script
-(function(){    
+
+(function(){
+
+//nav elements -> to  test api for change pano  
+	function makeNav(arg){
+
+	  for(i=0; i<arg.length; i++){
+	    var node = '<a href="#" class="btn btn-default btn-sm">'+i+'</a>';
+	    var $node =  $(node);
+	    $node.bind({
+	      click: function(e){
+	         world.updateScene(API.libCollection, e.currentTarget.text);          
+	      }
+	    });
+	    $('#world-nav').append($node);
+	  }
+	} 
 	makeNav(API.libCollection.libRecord);
 })();
