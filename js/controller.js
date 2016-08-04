0a3de3418429d61f274c9040314e53968ed412d6
salus-sage <salus.sage@gmail.com>
2016-08-04 22:26:45 +0530

    Refactor: #1 add player controls

--- js/controller.js
+++ js/controller.js
@@ -10,8 +10,87 @@
 //Events and handlers
 (function(){
   
- world.controller = function(){
- window.worldController = {}; 
+ function playerControls(){
+
+//Game control components - HTML & CSS UI events
+ window.worldToScreenEvents = {
+   zoom: function (x) {
+    if (world.cam.fov >=20 && world.cam.fov <=85 ) {
+    if (x == 0) {
+      world.cam.fov-=5;
+    }
+    else {
+      world.cam.fov+=5;
+    }
+    }
+    else if(world.cam.fov < 20 ){
+  world.cam.fov = 20;
+      }
+      else if(world.cam.fov > 85 ){
+  world.cam.fov = 85;
+      }
+    
+    world.cam.updateProjectionMatrix();
+    
+ },
+
+ fullScreen: function (x) {
+   
+     if (x == 0) {
+       document.getElementById('resizef').style.visibility = 'hidden';
+       document.getElementById('resizes').style.visibility = 'visible';
+       var element = document.getElementById('world-container');
+     // Supports most browsers and their versions.
+     var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+  console.log(requestMethod);
+     if (requestMethod) { // Native full screen.
+         requestMethod.call(element);
+     } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+         var wscript = new ActiveXObject("WScript.Shell");
+         if (wscript !== null) {
+             wscript.SendKeys("{F11}");
+         }
+     }
+     }
+     else if (x == 1) {
+       document.getElementById('resizef').style.visibility = 'visible';
+       document.getElementById('resizes').style.visibility = 'hidden';
+       var element = document;
+       var requestMethod = element.cancelFullScreen||element.webkitCancelFullScreen||element.mozCancelFullScreen||element.exitFullscreen;
+       console.log(requestMethod);
+        if (requestMethod) { 
+  // Native full screen.
+         requestMethod.call(element);
+        }
+        else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+         var wscript = new ActiveXObject("WScript.Shell");
+         if (wscript !== null) {
+             wscript.SendKeys("{F11}");
+         }
+       
+       //document.webkitCancelFullScreen();
+     }
+     }
+  },
+
+
+  autoRotate: function (x) {
+    if (x == 0) {
+      console.log('s');
+      document.getElementById('autorotateplay').style.visibility = 'hidden';
+      document.getElementById('autorotatepause').style.visibility = 'visible';
+      world.controls.autoRotate = true;
+    }
+    else if (x == 1) {
+      document.getElementById('autorotateplay').style.visibility = 'visible';
+      document.getElementById('autorotatepause').style.visibility = 'hidden';
+      world.controls.autoRotate = false;
+    }
+  }
+
+ }; 
+
+
 
    function bind(scope, func) {
     return function bound() {
@@ -28,80 +107,6 @@
   
 
   var mouse = new THREE.Vector2(), rotSpeed = 0.1;
-
-    
-  world.zoom = function (x) {
-    if (this.cam.fov >=20 && this.cam.fov <=85 ) {
-    if (x == 0) {
-      this.cam.fov-=5;
-    }
-    else {
-      this.cam.fov+=5;
-    }
-    }
-    else if(this.cam.fov < 20 ){
-	this.cam.fov = 20;
-      }
-      else if(this.cam.fov > 85 ){
-	this.cam.fov = 85;
-      }
-    
-    this.cam.updateProjectionMatrix();
-    
- };
- 
- world.fullScreen = function (x) {
-  
-    if (x == 0) {
-      document.getElementById('resizef').style.visibility = 'hidden';
-      document.getElementById('resizes').style.visibility = 'visible';
-      var element = document.getElementById('world-container');
-    // Supports most browsers and their versions.
-    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
-	console.log(requestMethod);
-    if (requestMethod) { // Native full screen.
-        requestMethod.call(element);
-    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
-        var wscript = new ActiveXObject("WScript.Shell");
-        if (wscript !== null) {
-            wscript.SendKeys("{F11}");
-        }
-    }
-    }
-    else if (x == 1) {
-      document.getElementById('resizef').style.visibility = 'visible';
-      document.getElementById('resizes').style.visibility = 'hidden';
-      var element = document;
-      var requestMethod = element.cancelFullScreen||element.webkitCancelFullScreen||element.mozCancelFullScreen||element.exitFullscreen;
-      console.log(requestMethod);
-       if (requestMethod) { 
-	// Native full screen.
-        requestMethod.call(element);
-       }
-       else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
-        var wscript = new ActiveXObject("WScript.Shell");
-        if (wscript !== null) {
-            wscript.SendKeys("{F11}");
-        }
-      
-      //document.webkitCancelFullScreen();
-    }
-    }
- };
- 
-  world.autoRotate = function (x) {
-    if (x == 0) {
-      console.log('s');
-      document.getElementById('autorotateplay').style.visibility = 'hidden';
-      document.getElementById('autorotatepause').style.visibility = 'visible';
-      this.controls.autoRotate = true;
-    }
-    else if (x == 1) {
-      document.getElementById('autorotateplay').style.visibility = 'visible';
-      document.getElementById('autorotatepause').style.visibility = 'hidden';
-      this.controls.autoRotate = false;
-    }
-  };
 }
  world.eventHandlers = {
     onDocumentMouseWheel: function (event) {
@@ -109,30 +114,30 @@
 
       // WebKit
       //fov limits = 20 and 85
-      if (this.cam.fov >=20 && this.cam.fov <=85 ) {
+      if (world.cam.fov >=20 && world.cam.fov <=85 ) {
   
       
       if ( event.wheelDeltaY ) {
-  this.cam.fov -= event.wheelDeltaY * 0.005;
+  world.cam.fov -= event.wheelDeltaY * 0.005;
       }
       // Opera / Explorer 9
       else if ( event.wheelDelta ) {
-  this.cam.fov -= event.wheelDelta * 0.005;
+  world.cam.fov -= event.wheelDelta * 0.005;
       }
       // Firefox
       else if ( event.detail ) {
-  this.cam.fov -= event.detail * 0.05;
+  world.cam.fov -= event.detail * 0.05;
       } 
       
       }
-      else if(this.cam.fov < 20 ){
-  this.cam.fov = 20;
+      else if(world.cam.fov < 20 ){
+  world.cam.fov = 20;
       }
-      else if(this.cam.fov > 85 ){
-  this.cam.fov = 85;
+      else if(world.cam.fov > 85 ){
+  world.cam.fov = 85;
       }
-      this.cam.updateProjectionMatrix();
-      console.log(this.cam.fov);
+      world.cam.updateProjectionMatrix();
+      console.log(world.cam.fov);
     },
       
     onClick: function (event) {
@@ -141,16 +146,16 @@
       
       mouse.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
       var raycaster = new THREE.Raycaster();
-      raycaster.setFromCamera( mouse, this.cam );
-      var intersects = raycaster.intersectObjects( this.scene.children );
+      raycaster.setFromCamera( mouse, world.cam );
+      var intersects = raycaster.intersectObjects( world.scene.children );
       if ( intersects.length > 0 ) {
   for (var i = 0; i < intersects.length; i++) {
    
       if (intersects[i].object.name === 'philly_object') {
         console.log(intersects[i].object.name);
-        this.controls.enabled = false;
-        this.trans_control.attach(intersects[i].object);
-        this.trans_control.enabled = true;
+        world.controls.enabled = false;
+        world.trans_control.attach(intersects[i].object);
+        world.trans_control.enabled = true;
         //display some help info
         info = '<small>Keys | t : translate | y : scale |' +
         ' r : rotate | u : quit </small>';
@@ -163,7 +168,7 @@
     if (intersects[i].object.name == name){
       
     sceneNum = k;
-    info = 'This is '+ k + ' Pano';
+    info = 'world is '+ k + ' Pano';
     
     document.getElementById('info').innerHTML = info;
         }
@@ -176,24 +181,24 @@
     
     onKeydown: function (event) {
       
-      var variable = this.sphere.rotation;
+      var variable = world.sphere.rotation;
       switch (event.keyCode) {
   
   case 85: // U
-    this.trans_control.enabled = false;
-    this.controls.enabled = true;
-    // is there something to detach this object? confirm it
-    this.trans_control.detach(this.trans_control.object);
+    world.trans_control.enabled = false;
+    world.controls.enabled = true;
+    // is there something to detach world object? confirm it
+    world.trans_control.detach(world.trans_control.object);
     document.getElementById('info').innerHTML = '';
     break;
   case 82: // R
-    this.trans_control.setMode("rotate");
+    world.trans_control.setMode("rotate");
     break;
   case 89: // Y
-    this.trans_control.setMode("scale");
+    world.trans_control.setMode("scale");
     break;
   case 84: // T
-    this.trans_control.setMode("translate");
+    world.trans_control.setMode("translate");
     break;
   //for rotation of panorama sphere
   case 65: // A
@@ -262,9 +267,9 @@ console.log(requestMethod);
 
  */
   //document.getElementById('container').webkitRequestFullScreen();
-  this.controls.enabled = false;
-        this.trans_control.attach(this.philly_selector);
-        this.trans_control.enabled = true;
+  world.controls.enabled = false;
+        world.trans_control.attach(world.philly_selector);
+        world.trans_control.enabled = true;
         //display some help info
        /* info = '<small>Keys | t : translate | y : scale |' +
         ' r : rotate | u : quit </small>';
@@ -285,12 +290,12 @@ console.log(requestMethod);
       }
     },
     onWindowResize: function (event) {
-      this.aspect_ratio = window.innerWidth / window.innerHeight;
-      this.cam.aspect = this.aspect_ratio;
-      this.cam.updateProjectionMatrix();
-      this.renderer.setSize(window.innerWidth, window.innerHeight);
-      this.render();
+      world.aspect_ratio = window.innerWidth / window.innerHeight;
+      world.cam.aspect = world.aspect_ratio;
+      world.cam.updateProjectionMatrix();
+      world.renderer.setSize(window.innerWidth, window.innerHeight);
+      world.render();
     }
 }
-world.controller();
+playerControls();
 })();
